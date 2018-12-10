4f906968d2443c311819a4ad05df24d8184571c1
Lukas <lukas.gudonis1989@gmail.com>
2018-12-10 10:42:52 +0200

    Second commit

--- springProject/src/main/java/config/SpringConfig.java
+++ springProject/src/main/java/config/SpringConfig.java
@@ -1,9 +1,44 @@
 package config;
 
+import dao.UserDao;
+import dao.UserDaoImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import service.UserService;
+import service.UserServiceImpl;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public UserDao getUserDao() {
+        return new UserDaoImpl(getJdbcTemplate());
+    }
+
+    @Bean
+    public UserService getUserService() {
+        return new UserServiceImpl();
+    }
 	
 
 }
